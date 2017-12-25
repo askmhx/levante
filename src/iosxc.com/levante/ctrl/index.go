407992ed3c0e6c4b3c63780a5cf7308430016397
Crazz <mhxdev@gmail.com>
2017-12-25 16:47:18 +0800

    improved start page

--- src/iosxc.com/levante/ctrl/index.go
+++ src/iosxc.com/levante/ctrl/index.go
@@ -16,8 +16,8 @@ func (this *IndexCtrl) IndexHandle(context context.Context) {
 	this.DB.Order("created_at DESC").Limit(5).Find(&postList)
 	context.ViewData("postList", postList)
 	type ColCount struct {
-		ColName   string
-		Count int
+		ColName string
+		Count   int
 	}
 
 	var mcList []ColCount
@@ -38,17 +38,42 @@ func (this *IndexCtrl) ArchiveHandle(context context.Context) {
 }
 
 func (this *IndexCtrl) StartHandle(context context.Context) {
-	linkGroupsDO := []orm.LinkGroup{}
-	this.DB.Order("sort DESC").Find(&linkGroupsDO)
-	linkGroupMap := map[string][]orm.Link{}
-	for _, linkGroup := range linkGroupsDO {
-		links := []orm.Link{}
-		this.DB.Model(&linkGroup).Related(&links).Order("sort ASC")
-		if (len(links) > 0) {
-			linkGroupMap[linkGroup.Title] = links
-		}
+
+	type LinkData struct {
+		Id          int
+		Title       string
+		Url         string
+		Image       string
+		Description string
+		Owner       string
+		Highlight   int
+		Gtitle      string
 	}
-	context.ViewData("linkGroupMap", linkGroupMap)
+	var linkList []LinkData
+	var sql = "select l.id,l.url,l.title as title,l.image,l.description,l.owner,l.highlight ,g.title as gtitle from links l left join link_groups g on l.link_group_id = g.id order by g.sort,l.sort";
+	this.DB.Raw(sql).Scan(&linkList);
+
+	type LinkGroupEntry struct {
+		Title    string
+		LinkList []LinkData
+	}
+
+	var linkGroupList []LinkGroupEntry
+
+	var groupEntry = LinkGroupEntry{Title: "", LinkList: []LinkData{}}
+
+	for _, link := range linkList {
+		if groupEntry.Title == "" || groupEntry.Title != link.Gtitle {
+			if groupEntry.Title != "" {
+				linkGroupList = append(linkGroupList, groupEntry)
+			}
+			groupEntry = LinkGroupEntry{Title: link.Gtitle, LinkList: []LinkData{}}
+		}
+		groupEntry.LinkList = append(groupEntry.LinkList, link)
+	}
+	linkGroupList = append(linkGroupList, groupEntry)
+
+	context.ViewData("linkGroupList", linkGroupList)
 	context.View("front/start.html")
 }
 
