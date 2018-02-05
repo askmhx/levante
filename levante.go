cdf42d6ecff2a2e4d1d2bf6a100780397aa299a7
Crazz <mhxdev@gmail.com>
2018-02-05 14:01:55 +0800

    levante remove wordpress api

--- levante.go
+++ levante.go
@@ -27,34 +27,7 @@ func initRoute(application *iris.Application) {
 	application.Get(url_about,  app.Handler(indexCtrl.AboutHandle))
 	application.Get(url_photo,  app.Handler(photoCtrl.IndexHandle))
 	application.Get(url_post,  app.Handler(postCtrl.ReadHandle))
-	wpApiInitRoute(application)
-
 }
-
-//WordPress v2 restful api urls
-
-const url_wp_base_url = "/wp/v2"
-const url_wp_api_posts = "/posts"
-const url_wp_api_revisions = "/revisions"
-const url_wp_api_categories = "/categories"
-const url_wp_api_tags = "/tags"
-const url_wp_api_pages = "/pages"
-const url_wp_api_comments = "/comments"
-const url_wp_api_taxonomies = "/taxonomies"
-const url_wp_api_media = "/media"
-const url_wp_api_user = "/users"
-const url_wp_api_types = "/types"
-const url_wp_api_statuses = "/statuses"
-const url_wp_api_settings = "/settings"
-
-//WordPress v2 restful api
-
-func wpApiInitRoute(app *iris.Application){
-	//wpApi := app.Party(url_wp_base_url)
-	//postsCtrl := wp.PostsCtrl{}
-	//wpApi.Any(url_wp_api_posts,postsCtrl)
-}
-
 
 func registerErrorHandler(application *iris.Application) {
 	application.OnErrorCode(iris.StatusNotFound,  app.Handler(func(ctx *app.Context) {
